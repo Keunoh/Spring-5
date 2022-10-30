c8bf449a62bb992996047b53bbfacabd6787dde6
Keunoh <rmsdhcjswo1@gmail.com>
2022-10-30 19:11:38 +0900

    Duplication Member Check

---
+++ src/main/java/book/spring5/spring03/DuplicateMemberException.java
@@ -0,0 +1,8 @@
+package book.spring5.spring03;
+
+public class DuplicateMemberException extends RuntimeException {
+
+    public DuplicateMemberException(String message) {
+        super(message);
+    }
+}
