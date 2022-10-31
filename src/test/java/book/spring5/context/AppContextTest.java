dfa793a7f328f9ccabeeaa3ab9c265c5dc999a14
Keunoh <rmsdhcjswo1@gmail.com>
2022-10-31 19:54:59 +0900

    Test code for Bean Exceptions

--- src/test/java/book/spring5/context/AppContextTest.java
+++ src/test/java/book/spring5/context/AppContextTest.java
@@ -1,8 +1,11 @@
 package book.spring5.context;
 
+import book.spring5.spring03.MemberDao;
+import book.spring5.spring03.MemberPrinter;
 import book.spring5.spring03.config.AppCtx;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.BeanNotOfRequiredTypeException;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -23,10 +26,16 @@ public class AppContextTest {
     }
 
     @Test
-    @DisplayName("등록되지 않은 이름으로 빈을 조회하면 exception발생")
+    @DisplayName("등록되지 않은 이름으로 빈을 조회하면 exception 발생")
     public void findExceptionWrongBeanName() {
-
         assertThrows(NoSuchBeanDefinitionException.class,
                 () -> applicationContext.getBean("memberDao1"));
+    }
+
+    @Test
+    @DisplayName("이름과 매칭되지 않는 클래스타입을 넘겼을 경우 exception 발생")
+    public void findExceptionWrongBeanType() {
+        assertThrows(BeanNotOfRequiredTypeException.class,
+                () -> applicationContext.getBean("memberDao", MemberPrinter.class));
     }
 }
