759e62d0916ebdc60357697acda8db36ccae9afc
Keunoh <rmsdhcjswo1@gmail.com>
2022-10-31 19:48:27 +0900

    Test code for NoSuchBeanDefinitionException

--- src/test/java/book/spring5/context/AppContextTest.java
+++ src/test/java/book/spring5/context/AppContextTest.java
@@ -1,10 +1,13 @@
 package book.spring5.context;
 
-import book.spring5.spring03.MemberRegisterService;
 import book.spring5.spring03.config.AppCtx;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class AppContextTest {
 
@@ -17,5 +20,13 @@ public class AppContextTest {
             String beanDefinitionName = beanDefinitionNames[i];
             System.out.println("beanDefinitionName = " + beanDefinitionName);
         }
+    }
+
+    @Test
+    @DisplayName("등록되지 않은 이름으로 빈을 조회하면 exception발생")
+    public void findExceptionWrongBeanName() {
+
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> applicationContext.getBean("memberDao1"));
     }
 }
